4383135bcddde5e6a54f1c7cbe72d09567809a14
Saketh Mahadevan <fizzfreshers@gmail.com>
2025-06-25 23:48:51 +0530

    Added Task.js to models

---
+++ server/models/Task.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String },
+    deadline: { type: Date },
+    status: { type: String, enum: ['pending', 'in-progress', 'completed'], default: 'pending' },
+    assignedTo: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // array of users (gets assigned to multiple users)
+    team: { type: mongoose.Schema.Types.ObjectId, ref: 'Team' }, // team this task belongs to
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Task', TaskSchema);
