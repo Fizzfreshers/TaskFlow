52cbf61e61fde36239e4d1fe29fc5ece2958560a
Saketh Mahadevan <fizzfreshers@gmail.com>
2025-06-25 23:54:39 +0530

    Made changes in server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,6 +12,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/tasks',taskRoutes);
 
 app.get('/', (req, res) => {
     res.send('Task Manager API is running!');
