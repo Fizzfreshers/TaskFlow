5591d44b871fe507649b6d56a42cb80262f19b73
Saketh Mahadevan <fizzfreshers@gmail.com>
2025-07-09 09:51:24 +0530

    Added roles in User.js

--- server/models/User.js
+++ server/models/User.js
@@ -6,7 +6,12 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     teams: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Team' }], //link to teams
-    isOnline: { type: Boolean, default: false } //online status
+    isOnline: { type: Boolean, default: false }, //online status
+    role : {
+        type: String,
+        enum: ['user','team-leader','admin'],
+        default: 'user'
+    }
 }, { timestamps: true });
 
 // hash password
