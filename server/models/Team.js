08aca00d81377f8f3688e2e02d9f9af588e1e3c8
Saketh Mahadevan <fizzfreshers@gmail.com>
2025-07-09 09:53:45 +0530

    Added leader role in Team.js

--- server/models/Team.js
+++ server/models/Team.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 const TeamSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
     members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // Users belonging to this team
-    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true } // The user who created the team
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // The user who created the team
+    leader:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        default: null
+    }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Team', TeamSchema);
