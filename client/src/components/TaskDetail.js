482dfded8a318e15bf1b58f0cd43793e48b4a470
Saketh Mahadevan <fizzfreshers@gmail.com>
2025-07-13 22:12:13 +0530

    Modified taskDetail.js

--- client/src/components/TaskDetail.js
+++ client/src/components/TaskDetail.js
@@ -14,7 +14,7 @@ const TaskDetail = ({ task }) => {
 
     return (
         <Box>
-            <Typography variant="h4" gutterBottom>{task.title}</Typography>
+            <Typography id="task-detail-title" variant="h4" gutterBottom>{task.title}</Typography>
             <Box sx={{ display: 'flex', gap: 2, alignItems: 'center', mb: 2 }}>
                 <Chip label={task.status} color={statusColors[task.status]} sx={{ textTransform: 'capitalize' }} />
                 <Typography variant="body1">
@@ -24,27 +24,27 @@ const TaskDetail = ({ task }) => {
             <Divider sx={{ mb: 2 }} />
             
             <Typography variant="h6">Description</Typography>
-            <Typography paragraph>{task.description || 'No description provided.'}</Typography>
+            <Typography id="task-detail-description" paragraph>{task.description || 'No description provided.'}</Typography>
             <Divider sx={{ mb: 2 }} />
             
             <Typography variant="h6">Assigned To</Typography>
             <List>
-                {task.assignedTo.map(user => (
+                {task.assignedTo && task.assignedTo.length > 0 ? task.assignedTo.map(user => (
                     <ListItem key={user._id}>
                         <ListItemAvatar><Avatar><PersonIcon /></Avatar></ListItemAvatar>
                         <ListItemText primary={user.name} secondary={user.email} />
                     </ListItem>
-                ))}
+                )) : <ListItem><ListItemText primary="No individual users assigned." /></ListItem>}
             </List>
 
             <Typography variant="h6">Assigned Teams</Typography>
             <List>
-                {task.teams.map(team => (
+                 {task.teams && task.teams.length > 0 ? task.teams.map(team => (
                     <ListItem key={team._id}>
                         <ListItemAvatar><Avatar><GroupIcon /></Avatar></ListItemAvatar>
                         <ListItemText primary={team.name} />
                     </ListItem>
-                ))}
+                )) : <ListItem><ListItemText primary="No teams assigned." /></ListItem>}
             </List>
         </Box>
     );
