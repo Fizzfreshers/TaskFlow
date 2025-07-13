231ef8366182051d83cfeb2856ab926d5af1886a
Saketh Mahadevan <fizzfreshers@gmail.com>
2025-07-13 22:18:07 +0530

    fixed a bug in taskRoutes.js

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose')
 const { protect } = require('../middleware/authMiddleware');
 const Task = require('../models/Task');
 const Notification = require('../models/Notifications');
